a2ff720b313240902465ead1f913a9a658499e9b
docNemo <varyag300300@yandex.ru>
2023-10-19 19:18:32 +0300

    init structure

---
+++ classifier-api/src/main/java/ru/mai/arachni/classifier/ArachniClassifierApplication.java
@@ -0,0 +1,12 @@
+package ru.mai.arachni.classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArachniClassifierApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ArachniClassifierApplication.class, args);
+    }
+}
