480319ca44c0a6a7e0e10e6b112ef3782a542b9d
docNemo <57809797+docNemo@users.noreply.github.com>
2023-10-19 20:23:29 +0300

    init structure (#1)

---
+++ classifier-api/src/main/java/ru/mai/arachni/classifier/controller/ClassifierController.java
@@ -0,0 +1,17 @@
+package ru.mai.arachni.classifier.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class ClassifierController {
+
+    @PostMapping
+    public String test() {
+        return "ok";
+    }
+}
