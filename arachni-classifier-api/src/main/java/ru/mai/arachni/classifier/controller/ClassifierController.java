1e7f3b5bf7725c7819d2fcb685969b5750fbe9f4
docNemo <varyag300300@yandex.ru>
2023-11-24 00:10:57 +0300

    feature/devslaves-972642 : add prod api

--- arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/controller/ClassifierController.java
+++ arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/controller/ClassifierController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/test")
+@RequestMapping("/classifier")
 public class ClassifierController {
 
-    @PostMapping
+    @PostMapping("/category")
     public String test() {
-        return "ok";
+        return "Это просто нечто";
     }
 }
