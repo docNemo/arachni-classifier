b99c6a7bb249e9a66a1c6178cb0cbf522a5d0730
docNemo <57809797+docNemo@users.noreply.github.com>
2023-11-24 00:19:35 +0300

    feature/devslaves-972642 (#8)
    * feature/devslaves-972642 : add prod api
    
    * feature/devslaves-972642 : add response

--- arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/controller/ClassifierController.java
+++ arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/controller/ClassifierController.java
@@ -4,14 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mai.arachni.classifier.dto.response.CategoryClassifierResponse;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/test")
+@RequestMapping("/classifier")
 public class ClassifierController {
 
-    @PostMapping
-    public String test() {
-        return "ok";
+    @PostMapping("/category")
+    public CategoryClassifierResponse getClassifiedCategory() {
+        return new CategoryClassifierResponse("Это просто нечто");
     }
 }
