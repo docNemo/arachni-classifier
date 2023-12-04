d196814ff94d6de917d1a4248935cf0df83bf4a4
docNemo <57809797+docNemo@users.noreply.github.com>
2023-12-04 18:55:48 +0300

    feature/devslaves-975463: add normalization (#11)

--- arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/configuration/ClassifierConfiguration.java
+++ arachni-classifier-api/src/main/java/ru/mai/arachni/classifier/configuration/ClassifierConfiguration.java
@@ -1,11 +1,16 @@
 package ru.mai.arachni.classifier.configuration;
 
+import company.evo.jmorphy2.MorphAnalyzer;
+import company.evo.jmorphy2.ResourceFileLoader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import ru.mai.arachni.classifier.normalizer.TextNormalizer;
 import ru.mai.arachni.classifier.provider.ModelProvider;
 import ru.mai.arachni.classifier.service.ClassifierService;
+
+import java.io.IOException;
 
 @Configuration
 public class ClassifierConfiguration {
@@ -24,10 +29,32 @@ public class ClassifierConfiguration {
 
     @Bean
     public ClassifierService classifierService(
-            ModelProvider modelProvider
+            ModelProvider modelProvider,
+            TextNormalizer textNormalizer
     ) {
         return new ClassifierService(
-                modelProvider
+                modelProvider,
+                textNormalizer
         );
+    }
+
+    @Bean
+    public TextNormalizer textNormalizer(
+            MorphAnalyzer morphAnalyzer
+    ) {
+        return new TextNormalizer(
+                morphAnalyzer
+        );
+    }
+
+    @Bean
+    public MorphAnalyzer morphAnalyzer(
+            @Value("${morphy.path}") String path
+    )
+            throws IOException {
+        return new MorphAnalyzer.Builder()
+                .fileLoader(new ResourceFileLoader(path))
+                .charSubstitutes(null)
+                .build();
     }
 }
